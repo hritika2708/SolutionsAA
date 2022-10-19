16c59d582cc3900bfa9610d712c5d795c7e82c1e
hritika2708 <43510108+hritika2708@users.noreply.github.com>
2022-10-19 18:47:46 +0530

    modified Code clean code

--- profanity_degree.py
+++ profanity_degree.py
@@ -7,18 +7,20 @@ import re
 
 df=pd.read_csv('filename.csv')   
 df['racism_degree'] = 0 #let it be last column
+racist = ...
 
 def clean():
-	'''
-	Function to clean tweet by removing links, special characters, etc. by using regex
-	It returns a tokenised data
-	'''
-    
+    '''
+    Function to clean tweet by removing links, special characters, etc. by using regex
+    It returns a tokenised data
+    '''
     def clean_tweet(tweet):
-    	'''
-    	This is a utility function, created to improve readability
-    	which can be changed based on requirement
-    	'''
+        '''
+        This is a utility function, created to improve readability
+        which can be changed based on requirement
+        '''
+        #after substitution the text is split by spaces as delimiter, multiple whitespace is considered as one
+        #they are then joined back into a list using join function
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
     #let the name of the column that stores the tweets be 'tweets' and it is the first column of the dataset
@@ -30,7 +32,7 @@ def racism(tweet):
     #let the given racial slurs be present in a global set called racist
     #let r be an integer which will store the number of slur words used in each tweet
     #I assume that the racist word used will be written as a whole without censoring
-    tweet=tweet.split(" ")
+    tweet = tweet.split()
     r=0
     for i in tweet:
         if i in racist:
@@ -39,7 +41,7 @@ def racism(tweet):
     return (r*100)/len(tweet)        
 
 
-clean()
+text = clean()
 
 for i in range(len(df)):
-     df.iloc[i,-1]=racism(df.iloc[i,-1])
+    df.iloc[i,-1]=racism(df.iloc[i,-1])
